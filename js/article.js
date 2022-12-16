3edf046b575f884cb30fc660188b5857389ab6e8
“vicky” <s6102161021@gmail.com>
2022-12-16 20:47:00 +0800

    fix: db.json

--- js/article.js
+++ js/article.js
@@ -1,12 +1,13 @@
 const _heroSection = document.querySelector('.heroSection');
 const _artTop = document.querySelector('.article__top');
+const _artMid = document.querySelector('.article__middle');
+const _artMap = document.querySelector('.article__mapAndTags');
 let _data = [];
 let _week = '';
 let _article = '';
 
 function init() {
   getData();
-  renderLeaflet();
 }
 init();
 
@@ -20,15 +21,15 @@ function getData() {
       renderHtml();
     })
     .catch(function (err) {
-      swal({
-        title: 'Σ(ﾟдﾟ) 哇糟糕了',
-        text: '找不到文章，帶你回首頁',
-        icon: 'warning',
-        button: '確定',
-        className: 'swalBtn',
-      }).then(function () {
-        window.location.href = 'index.html';
-      });
+      // swal({
+      //   title: 'Σ(ﾟдﾟ) 哇糟糕了',
+      //   text: '找不到文章，帶你回首頁',
+      //   icon: 'warning',
+      //   button: '確定',
+      //   className: 'swalBtn',
+      // }).then(function () {
+      //   window.location.href = 'index.html';
+      // });
     });
 }
 
@@ -36,67 +37,8 @@ function renderHtml() {
   changeHeadContent(_article);
   _heroSection.innerHTML = createHeroSection(_article);
   _artTop.innerHTML = createArtTop(_article);
-}
-
-$(function () {
-  // 點擊 本週景點動畫
-  $('.catalogBtn').click(function (event) {
-    $(this).find('i').toggleClass('active');
-    $(this).parent().find('.catalogDest').slideToggle('slow');
-  });
-});
-
-function renderLeaflet() {
-  const _blackIcon = new L.Icon({
-    iconUrl:
-      'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png',
-    shadowUrl:
-      'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41],
-  });
-
-  let map = L.map('map', {
-    center: [25.068356194024172, 121.52481353493157],
-    zoom: 14,
-    zoomControl: false,
-  });
-
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    // OSM的圖磚資料.addTo加入到(map裡面去)
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | 台北市中山區 ',
-  }).addTo(map);
-
-  const data = [
-    {
-      name: '丸林滷肉飯',
-      rate: 3.9,
-      mapUrl: 'https://goo.gl/maps/hbX1HYq1oJ4qDMbL6',
-      local: [25.068780763626144, 121.52490147444489],
-    },
-    {
-      name: 'Orion 沖繩啤酒節',
-      rate: 4.2,
-      mapUrl: 'https://goo.gl/maps/7LZvGnEm3UBKjj6p6',
-      local: [25.07189941449988, 121.5222065131828],
-    },
-  ];
-
-  data.forEach((item) => {
-    // 添加標記點
-    L.marker(item.local, { icon: _blackIcon })
-      .addTo(map)
-      .bindPopup(
-        `<div class="popupContent">
-          <h4>${item.name}</h4>
-          <p>不專業評價 ${item.rate}</p>
-          <a href="${item.mapUrl}" target="_blank" class="btn btnPrimary">前往 <i class="fa-solid fa-angle-right"></i></a>
-        </div>`
-      );
-  });
+  createArtMid(_article);
+  createArtMap();
 }
 
 function getUrlParameter(parameter) {
@@ -130,11 +72,72 @@ function createHeroSection(data) {
   return content;
 }
 
-function createArtTop(data){
-  let content = `<div class="article__top">
+function createArtTop(data) {
+  let content = `
             <h2>${data.title}</h2>
             <p class="article__top__date">written by ${data.userName} ｜ ${data.writtenDate}</p>
-            <p class="article__top__brief">${data.briefing}</p>
-          </div>`;
+            <p class="article__top__brief">${data.briefing}</p>`;
   return content;
+}
+
+function createArtMid(data) {
+  _artMid.innerHTML = _article.content;
+
+  $('.catalogBtn').click(function (event) {
+    $(this).find('i').toggleClass('active');
+    $(this).parent().find('.catalogDest').slideToggle('slow');
+  });
+}
+
+function createArtMap() {
+  let data = _article.hashTags;
+  let hashTags = '';
+  data.forEach(function (item) {
+    let content = `<li>#${item}</li>`;
+    hashTags += content;
+  });
+
+  _artMap.innerHTML = `<h5>本週景點地圖</h5>
+            <div id="map"></div>
+            <ul>${hashTags}</ul>`;
+  renderLeaflet(_article);
+}
+
+function renderLeaflet(data) {
+  const destinations = data.destinations;
+  const _blackIcon = new L.Icon({
+    iconUrl:
+      'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png',
+    shadowUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+  });
+
+  let map = L.map('map', {
+    center: [destinations[0].local[0], destinations[0].local[1]],
+    zoom: 14,
+    zoomControl: false,
+  });
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | ${
+      data.city + data.district
+    } `,
+  }).addTo(map);
+
+  destinations.forEach((item) => {
+    // 添加標記點
+    L.marker(item.local, { icon: _blackIcon })
+      .addTo(map)
+      .bindPopup(
+        `<div class="popupContent">
+          <h4>${item.name}</h4>
+          <p>不專業評價 ${item.rate}</p>
+          <a href="${item.mapUrl}" target="_blank" class="btn btnPrimary">前往 <i class="fa-solid fa-angle-right"></i></a>
+        </div>`
+      );
+  });
 }
